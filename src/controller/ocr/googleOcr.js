402717a8c8f6f06eb3612f9354cd55c2480eed8d
Abdalstar Abdo <abdalstar@storkhost.com>
2023-12-18 21:16:19 +0200

    save

--- src/controller/ocr/googleOcr.js
+++ src/controller/ocr/googleOcr.js
@@ -10,11 +10,18 @@ module.exports = async (req, res) => {
     const file = req.files.image;
     const fileBase64 = fs.readFileSync(file.path, { encoding: 'base64' });
    const {fullTextAnnotation: {text: googleOcrTextRes}} = (await googleOcr(fileBase64)).responses[0];
+   
+    if(!googleOcrTextRes){
+      res.status(422).json({
+        message: "The data was not recognized correctly.",
+        status: 422,
+      })
+    }
 
    const result = formatter.googleOcrFormatter(googleOcrTextRes)
    const isValid = validation.googleOcrValidation(result);
-   console.log("🚀 ~ file: googleOcr.js:16 ~ module.exports= ~ isValid:", isValid)
-   console.log("🚀 ~ file: googleOcr.js:18 ~ module.exports= ~ result:", result)
+
+
    if(isValid){
      res.status(200).json(result)
    }else {
