f778e01cdcb1513c38d381080a5dc7135b74e5b8
Abdalstar Abdo <abdalstar@storkhost.com>
2023-12-20 16:33:28 +0200

    save

--- src/utils/formatter/googleOcrFormatter.js
+++ src/utils/formatter/googleOcrFormatter.js
@@ -5,7 +5,7 @@ module.exports = (text) => {
     var card_id = text.match(/\d{4}[-\s]?\d{4}[-\s]?\d{4}[-\s]?\d{4}/g);
     card_id = card_id ? card_id[0] : null
     
-    var expiry_date = text.match(/(0[1-9]|[0-2])\/([0-9][0-9]|2[0-1])/g);
+    var expiry_date = text.match(/(0[1-9]|1[0-2])\/(0[1-9]|[1-9][0-9])/g);
     expiry_date = expiry_date ? expiry_date[0] : null
     return {
         card_id,
